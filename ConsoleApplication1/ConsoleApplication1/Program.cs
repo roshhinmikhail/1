bca67bdaff062905dc46aaba9fdfed0cdb4ddbba
Mikhail <roshinmikhail2016@yandex.ru>
2016-11-13 18:02:39 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int u;
+            Console.WriteLine("Введите число");
+            u = Convert.ToInt32(Console.ReadLine());
+            if (u > 0) { u = u * 2; Console.WriteLine("Число положительное, увеличиваем в 2 раза"); }
+            else if (u < 0) { u = u - 3; Console.WriteLine("Число отрицательное, уменьшаем на 3"); }
+            else { u = 10; Console.WriteLine("Замена на 10"); }
+            Console.WriteLine("Результат: " + u);
+            Console.ReadLine();
+        }
+    }
+}
